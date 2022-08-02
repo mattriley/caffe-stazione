7b3b3a2491b8fc1f2e90cd3b5678f5c5ddf9de41
Matt Riley <mattrileyaus@gmail.com>
2022-08-03 04:57:52 +1000

    Unspecified changes

--- src/web-components.js
+++ src/web-components.js
@@ -1,13 +1,13 @@
 const compose = require('./compose');
 
 const composition = compose();
-const { webComponents } = composition.modules;
-if (!window.apps) window.apps = {};
-window.apps.caffestazioneWebcomponents = composition;
+if (!window.apps) window.apps = [];
+window.apps.push({ caffestazioneWebcomponents: composition });
 
 const fonts = document.createElement('link');
 fonts.href = 'https://fonts.googleapis.com/css?family=Roboto+Condensed|Shadows+Into+Light';
 fonts.rel = 'stylesheet';
 document.head.appendChild(fonts);
 
+const { webComponents } = composition.modules;
 customElements.define('stazione-header', webComponents.StazioneHeader);
